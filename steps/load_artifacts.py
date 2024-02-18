eba9feb5abea92eb57a26076521c12d5fc67404c
kameshrsk <kamesh030301@gmail.com>
2024-02-18 11:15:18 +0530

    Updated load_artifacts.py

--- steps/load_artifacts.py
+++ steps/load_artifacts.py
@@ -1,4 +1,3 @@
-from zenml import step
 from zenml.client import Client
 from torch.utils.data import DataLoader
 from typing import Tuple, Union, Annotated
@@ -13,19 +12,15 @@ def load_artifact_from_pipeline(pipeline_name: str, step_name: str, output_name:
     artifact = Client().get_artifact_version(artifact_id)
     return artifact.load()
 
-@step(enable_cache=False)
 def load_training_batch() -> DataLoader:
     return load_artifact_from_pipeline('data_pipeline', 'load_data', 'training_batch')
 
-@step(enable_cache=False)
 def load_testing_batch() -> DataLoader:
     return load_artifact_from_pipeline('data_pipeline', 'load_data', 'testing_batch')
 
-@step(enable_cache=False)
 def load_trained_model() -> BertForSequenceClassification:
     return load_artifact_from_pipeline('model_training_pipeline', 'train_model', 'output')
 
-@step(enable_cache=False)
 def load_scores() -> Tuple[float, float]:
     accuracy = load_artifact_from_pipeline('model_evaluation_pipeline', 'evaluate_model', 'accuracy')
     f1_score = load_artifact_from_pipeline('model_evaluation_pipeline', 'evaluate_model', 'f1')
